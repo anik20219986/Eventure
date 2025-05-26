b867f599bdfef0a3ada756af4741fc4d10db52f4
anik20219986 <anik.krishna20@gmail.com>
2025-05-26 19:58:59 +0530

    Add files via upload

---
+++ Frontend/src/components/Card/card.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import pic from "D:\COLLEGE\BLACKBUGS\Eventure\Frontend\src\assets\images\bk1.png"
+import "D:\COLLEGE\BLACKBUGS\Eventure\Frontend\src\components\Card\card.css"
+
+const Anik = () =>{
+
+}
+
+const Card= ({data}) => {
+  
+  return (
+    <div className='s justify-center items-center mx-14 w-100 h-96 my-8 font-openSans cursor-pointer'>
+    <div className='relative'>
+
+    <img src={`data:${data.img.contentType};base64,${data.img.data.data.toString('base64')}`} className='shadow-lg shadow-black h-72 rounded-tl-3xl w-96 items-center rounded-br-3xl'/>
+    <div className='absolute top-0 left-0 px-6 py-4'>
+    </div>
+    </div>
+    <h3 className='c mx-9 my-3 font-openSans text-2xl'>{data?.eventName}</h3>
+    <div className='mx-6 a text-orange-600'>
+     {data?.description}
+     <br></br>
+     {data?.date?.split('T')[0]}
+    </div>
+    </div>
+  )
+}
+export default Card;
